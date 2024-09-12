7f10fb0441e1fbb9b5d537d27847128b7291c525
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-09-12 21:32:03 +0100

    Addition of 404page.html
    Changes:
    - addition of 404page.html
    - changed app.js image link for usage on the 404page.html
    - changed background design
    - added 404page.html link to the navbar
    - position of second-section text
    - added a bar-line for the background
    - updated footer and linked the 404page.html to them

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
     for(const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
+        fetch("https://cataas.com/cat?position=center")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
-            image.width = 100;
-            image.height = 100;
+            image.width = 300;
+            image.height = 300;
         })
     }
 

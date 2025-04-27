bec609f914ae2427725b38609a9988395783edd4
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-04-27 10:33:59 +0100

    Removed template elements and favicon
    Changes:
    - Removed template css and .jsx components.
    - added a background and loader
    - added Main.jsx
    - added custom favicon and title

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,22 @@
+
+import './Main.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main_container">
+        <div class="wrapper">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <div class="shadow"></div>
+          <div class="shadow"></div>
+          <div class="shadow"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
